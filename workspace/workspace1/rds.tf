0e6084e6937638c258cba41ad5c9c4bf3c4935a7
sanket0770 <87951759+sanket0770@users.noreply.github.com>
2023-11-23 13:04:49 +0000

    Update rds.tf

--- workspace/workspace1/rds.tf
+++ workspace/workspace1/rds.tf
@@ -39,7 +39,7 @@ resource "aws_db_instance" "default" {
   db_name                       = "mydb1"
   engine                        = "mysql"
   engine_version                = "5.7"
-  identifier                    = "research-rds"   
+  identifier                    = "research-rds1"   
   instance_class                = "db.t3.micro"
   network_type                  = "IPV4"
   port                          = "3306" 
