d65faabb0d175ecdac7232c9edb80920df36a93b
sanket0770 <87951759+sanket0770@users.noreply.github.com>
2023-12-07 17:12:45 +0000

    Update eb.tf

--- workspace/workspace2/eb.tf
+++ workspace/workspace2/eb.tf
@@ -6,31 +6,34 @@ terraform {
     }
   }
 }
+
 provider "aws" {
   region = "eu-west-2"
   access_key = "AKIAX3LNWYOGIVRPHOXY"
   secret_key = "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"
 }
+
 resource "aws_elastic_beanstalk_application" "my_app" {
   name = "MyElasticBeanstalkAppsanket0001111"
 }
 
 resource "aws_elastic_beanstalk_environment" "my_environment" {
-  name        = "MyElasticBeanstalkAppsanket0001111"
   name        = "MyEnvironmentsanket0001111"
   application = aws_elastic_beanstalk_application.my_app.name
   solution_stack_name = "64bit Amazon Linux 2 v3.5.9 running Python 3.8"
-
+  
   setting {
     namespace = "aws:autoscaling:launchconfiguration"
     name      = "InstanceType"
     value     = "t2.micro"
   }
+
   setting {
     namespace = "aws:elasticbeanstalk:environment"
     name      = "EnvironmentType"
     value     = "SingleInstance"
   }
+
   setting {
     namespace = "aws:elasticbeanstalk:application:environment"
     name      = "PYTHONPATH"
@@ -42,3 +45,5 @@ resource "aws_elastic_beanstalk_environment" "my_environment" {
       value     = "aws-elasticbeanstalk-ec2-role"
     }
 }
+
+# You can add additional resources like AWS RDS, S3, etc., based on your application requirements.
