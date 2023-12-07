4f9bda25567023b3c1a10da3d34d51a01c99951c
sanket0770 <87951759+sanket0770@users.noreply.github.com>
2023-12-07 18:13:27 +0000

    Update pscript.py

--- pscript.py
+++ pscript.py
@@ -11,6 +11,7 @@ def deploy_environment(workspace):
 
         # Run Terraform commands for deployment
         subprocess.run(["terraform", "init", "-input=false"], check=True, shell=True, cwd=os.path.join(script_dir, workspace))
+        subprocess.run(["terraform", "plan", "-input=false"], check=True, shell=True, cwd=os.path.join(script_dir, workspace))
         subprocess.run(["terraform", "apply", "-auto-approve", "-lock=false"], check=True, shell=True, cwd=os.path.join(script_dir, workspace))
     except Exception as e:
         print(f"Error deploying workspace {workspace}: {e}")
